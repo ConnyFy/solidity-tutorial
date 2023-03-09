8b47f5a1083e6b14c95d8e5bbbd2ac3990c15bc2
ConnyFy <connyfy@gmail.com>
2023-03-10 00:06:08 +0100

    Implement struct demo

--- Types.sol
+++ Types.sol
@@ -262,12 +262,6 @@ contract SolidityTypes {
     }
 
     // # Compound
-    struct StructType {
-        address addr;
-        uint128 number;
-    }
-    StructType struct_variable;
-
     // Demo Enum
     enum Status { DRAFT, SENT, DELIVERED }
 
@@ -304,6 +298,27 @@ contract SolidityTypes {
     }
 
     using { addPrice } for Price;
+
+    // Struct demo
+    enum EyeColor { BROWN, BLUE, GREEN, HAZAL, OTHER }
+    struct Person {
+        uint16 height;
+        uint16 weight;
+        address account;
+        EyeColor eyeColor;
+    }
+
+    function struct_demo() public view {
+        Person memory person;
+        console.log(person.height);
+        
+        Person memory person2 = Person(190, 85, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, EyeColor.BROWN);
+        console.log(person2.height);
+        person2.height = 195;
+        console.log(person2.height);
+
+        // No recursive struct
+    }
 
     // # Dynamic
     int[5] int_array_variable;
