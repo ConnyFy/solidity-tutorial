1d7e5ab77e48593cd63898c67db544607fdff60f
ConnyFy <connyfy@gmail.com>
2023-03-16 23:52:52 +0100

    Implement mapping demo

---
+++ 1_data_types/11_Mapping.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "hardhat/console.sol";
+
+
+contract MappingDemo {
+
+    mapping(uint8 => uint256) mapping_var;
+
+    function demo() public {
+        // KeyType can only be elementary value types, user-defined value types, enums, and contract types
+        // ValueType can be any type including other mappings
+
+        // Interesting type, not something one would see in other languages.
+
+        // Operations
+        // Addition
+        mapping_var[10] = 123456789;
+        mapping_var[15] = 121212;
+
+        // Getter
+        uint256 value = mapping_var[15]; // 121212
+
+        // Deletion
+        delete mapping_var[10];
+        delete mapping_var[1]; // Keys do not need to exist
+
+        // Check if a key exist
+        // Unfortunatelly there is no way.
+        // The best we can do if the value of the key is different than 0.
+        // Although this would not detect if a key was purposely set to 0.
+        bool keyExist = mapping_var[15] != 0;
+
+        // So technically deletion could be written as
+        mapping_var[10] = 0;
+
+        // Get a list of keys -> NOT POSSIBLE!
+
+        // It can only be declared as a storage variable.
+        // It can only be part of a function header if the function is private/internal
+    }
+
+    // Nested mappings
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    function fc_with_mapping(mapping(uint8 => uint256) storage mapping_param) internal pure {
+
+    }
+}
