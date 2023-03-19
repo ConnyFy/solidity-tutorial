a6141ed977bb8642853a70168b053032be652ef9
ConnyFy <connyfy@gmail.com>
2023-03-19 22:38:02 +0100

    Implement declaration demo

---
+++ part2/1_variables/1_Declaration.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "hardhat/console.sol";
+
+
+contract DeclarationDemo {
+
+    function demo() public view {
+        // Declaration
+        // type name;
+        // type name = initial_value;
+        uint number1; // default value
+        uint number2 = 123123;
+        (uint number3, uint number4) = (33, 44);
+
+        // Assignment
+        //uint number2 = 456456; // Identifier already declared
+        number2 = 456456;
+        (number1, number2) = (1, 2);
+        (number1, number2) = (number2, number1);
+        console.log(number1, number2); // Outputs 2, 1
+        //(uint number5, number1) = (55, 11); // Cannot mix declaration and assignment
+
+        // Scoping
+        uint outer_scoped = 10;
+        {
+            outer_scoped = 20;
+            uint inner_scoped = 20;
+        }
+        outer_scoped = 30;
+        //inner_scoped = 30; // Undeclared identifier
+
+        {
+            uint inner_scoped = 40;
+        }
+
+        {
+            int8 inner_scoped = 50;
+        }
+
+        // To save stack-depth
+        uint final_result1;
+        uint final_result2;
+        {
+            (uint something, uint complex, uint calculation) = (1,2,3);
+            final_result1 = something+complex+calculation;
+            final_result2 = something*complex*calculation;
+        }
+    }
+}
