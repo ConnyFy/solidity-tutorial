41c121a430f6f18bcfa5a18eef87eced7963b24a
Connyfy <connyfy@gmail.com>
2024-01-23 23:39:26 +0100

    Update Storage Intro

--- 4_data_location/1_storage/1_Storage_Intro.sol
+++ 4_data_location/1_storage/1_Storage_Intro.sol
@@ -30,13 +30,12 @@ contract StorageIntroduction {
         owe[0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF] = 10;
     }
 
-    function a_checkValues() public view {
+    function a_checkValues() public {
         console.log("one:", one);
         console.log("ten:", ten);
         console.log("answers:", answers[0], answers[1]);
         console.log("numbers last element:", numbers[numbers.length-1]);
         console.log("owe:", owe[0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF]);
-
     }
 
     /*
@@ -44,7 +43,7 @@ contract StorageIntroduction {
     - Value types are always copied. You can only modify them via their original name, you cannot have pointers/references to them.
     - You can only have references to data types.
     - Storage variables declared in the body of a function are just pointers to an actual storage variable.
-    - You cannot assign new value to them, nor memory values. You can only assign real storage variables.
+    - You cannot assign new value to them, nor memory/calldata values. You can only assign real storage variables.
     */
     
     string pie = "apple pie";
@@ -67,49 +66,56 @@ contract StorageIntroduction {
         console.log(dessert); // Prints "apple pie"
     }
 
+    uint[] oldNumbers = [1, 2, 3];
+
     function c_pointerDemo2() public {
-        uint[] storage newNumbers = numbers;
-        newNumbers.push(4); // Both newNumbers and numbers will have '4' as their last element.
-        console.log("numbers[3]:", numbers[3], "length:", numbers.length);
+        uint[] storage newNumbers = oldNumbers;
+        newNumbers.push(4); // Both newNumbers and oldNumbers will have '4' as their last element.
+        console.log("oldNumbers[3]:", oldNumbers[3], "length:", oldNumbers.length);
         console.log("newNumbers[3]:", newNumbers[3], "length:", newNumbers.length);
 
         // newNumbers = [9,8,7]; // We cannot assign a newly created value to a storage reference.
-        numbers = [9,8,7]; // However, we can assign it to the original storage variable. Note that the change will be reflected by newNumbers.
-        console.log("numbers[0]:", numbers[0], "length:", numbers.length);
-        console.log("newNumbers[0]:", newNumbers[0], "length:", numbers.length);
+        oldNumbers = [9,8,7]; // However, we can assign it to the original storage variable. Note that the change will be reflected by newNumbers.
+        console.log("oldNumbers[0]:", oldNumbers[0], "length:", oldNumbers.length);
+        console.log("newNumbers[0]:", newNumbers[0], "length:", newNumbers.length);
 
         // delete newNumbers; // We cannot call delete on a storage reference, as it would create a dangling reference.
-        delete numbers; // However, we can call delete on the original storage variable. Remember, delete is just simple "reset" the variable. In this case, sets is length to 0.
-        console.log("numbers length:", numbers.length);
+        delete oldNumbers; // However, we can call delete on the original storage variable. Remember, delete is just simple "reset" the variable. In this case, sets is length to 0.
+        console.log("oldNumbers length:", oldNumbers.length);
         console.log("newNumbers length:", newNumbers.length);
     }
 
     /*
+    Parameters
     - Storage parameters are similar to storage pointers.
     - It means you need to pass a storage variable of that type as parameter.
     - Because of that, only internal or private functions can have storage parameters. (We will explain in the next lecture what internal and external calls are.)
     - The purpose of these parameters is if you explicitly want to perform an operation on an arbitrary storage variable, but you want to decide at the time of the calling, on which storage variable you want to perform it.
     */
-    function d_parameterDemo(string storage favoriteDessert) internal {
+    function d_printDessert(string storage favoriteDessert) internal {
         console.log("My favorite dessert is", favoriteDessert);
     }
 
-    function d_callParameterDemo() public {
+    function d_favoriteDesserts() public {
         d_parameterDemo(pie);
         d_parameterDemo(cake);
     }
 
-    function e_addElement(uint[] storage array, uint newElement) internal {
+    // Just like in case of storage pointers, you can modify the original data through the parameter.
+    function e_addElementToArray(uint[] storage array, uint newElement) internal {
         array.push(newElement);
-        console.log("New length from addElement:", array.length);
+        console.log("New length from addElement:", array.length, "new element:", newElement);
     }
-    function e_fillArray() public {
-        console.log("Length of original numbers:", numbers.length);
-        e_addElement(numbers, 0);
-        e_addElement(numbers, 1);
-        e_addElement(numbers, 2);
-        e_addElement(numbers, 3);
-        e_addElement(numbers, 4);
-        console.log("Length of original numbers:", numbers.length);
+
+    uint[] odd;
+    uint[] even;
+    function e_fillArrays() public {
+        e_addElement(odd, 1);
+        e_addElement(even, 2);
+        e_addElement(odd, 3);
+        e_addElement(even, 4);
+        e_addElement(odd, 5);
+        console.log("Length of odd numbers:", odd.length);
+        console.log("Length of even numbers:", even.length);
     }
 }
